fec02a985b86ebba50bbd87a90d454f01431bb0c
Aharon Bell <abell0538@gmail.com>
2020-07-09 21:54:09 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,10 @@
-def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
+ def rescue_princess_peach
+   status = "rescued"
+   puts "Hooray! Mario has rescued Princess Peach."
+ end
+ 
+ rescue_princess_peach(spec_helper)
+  puts "Hooray! #{spec_helper}"
 end
-
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
+  
+  
